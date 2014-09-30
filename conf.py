f1578368c197cc11b36fc10f950c2a1e6bd0fdf0
Nicolas <nulysse4@gmail.com>
2014-09-29 20:54:53 -0400

    Added world save folder

--- conf.py
+++ conf.py
@@ -1,3 +1,4 @@
 __author__ = 'Nicolas'
 
 server_full_path = r'C:\minecraft\minecraft_server.1.8.exe'
+world_store_path = None
--- mineserv.py
+++ mineserv.py
@@ -1,6 +1,20 @@
 __author__ = 'Nicolas'
 import subprocess
 import os.path
+import platform
+import tkFileDialog, Tkinter
 
+__worlds_directories = {('Windows','7') : r'%appdata%\.minecraft\saves'}
 def start_server(exe_path):
     subprocess.call(exe_path, cwd=os.path.dirname(os.path.abspath(exe_path)))
+
+def get_worlds_directory():
+    try:
+        return os.path.expandvars(__worlds_directories[(platform.system(), platform.release())])
+    except Exception:
+        root = Tkinter.Tk()
+        root.withdraw()
+        return tkFileDialog.askdirectory(parent=root, initialdir="/", title='Please select a directory')
+
+if __name__ == '__main__':
+    print get_worlds_directory()
