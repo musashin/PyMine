5e1d842081e718a21dc5b8b546e25e9ec2d6acf7
Nicolas <nulysse4@gmail.com>
2014-11-22 17:39:44 -0500

    Fixed server properties routine

--- server_prop.py
+++ server_prop.py
@@ -9,36 +9,45 @@ class ServerProp:
         self.lines = list()
 
     def load(self):
-        lineFormat = re.compile("""
-                                (?P<property>[\w\-]*)    #property
-                                [\s]*[=]*[\s]*
-                                (?P<value>[\w]+)*        #value
-                                [\s]*
-                                [(#|!)]*
-                                (?P<comment>[\w\s]*)     #comment
+        line_format = re.compile("""
+                                ^(?P<property>[\w\-]*)    #property
+                                [\s]*=*[\s]?
+                                (?P<value>[\w\s]*)        #value
+                                ([(#|!)]
+                                (?P<comment>[\w\s]*))?$     #comment
                                 """, re.VERBOSE)
         with open(self.path, 'r') as configFile:
             for line in configFile:
-                content = lineFormat.match(line)
-                self.lines.append([content.group('property'),
-                                   content.group('value'),
-                                   content.group('comment')])
-                #print line
-                #print '\t var: ' + str(lineFormat.match(line).group('property'))
-                #print '\t val: ' + str(lineFormat.match(line).group('value'))
-                #print '\t comment: ' + str(lineFormat.match(line).group('comment'))
+                content = line_format.match(line)
+                if content:
+                    self.lines.append([content.group('property'),
+                                       content.group('value'),
+                                       content.group('comment')])
+                    #print line
+                    #print '\t var: ' + str(line_format.match(line).group('property'))
+                    #print '\t val: ' + str(line_format.match(line).group('value'))
+                    #print '\t comment: ' + str(line_format.match(line).group('comment'))
 
     def get(self, property_name):
         return [val for prop, val, comment in self.lines if prop == property_name][0]
 
     def set(self, property_name, value):
         index = [index for index, prop in enumerate(self.lines) if prop[0] == property_name][0]
-        self.lines[index][1] = value
+        self.lines[index][1] = value + '\n'
 
-
+    def save(self):
+        with open(self.path+'x', 'w') as configFile:
+            for line in self.lines:
+                if line[0] and line[1]:
+                    configFile.write("{}={}".format(line[0], line[1]))
+                    if line[2]:
+                        configFile.write(" # {}".format(line[2]))
+                    else:
+                        pass
+                elif line[2]:
+                    configFile.write("# {}".format(line[2]))
 if __name__ == '__main__':
     test = ServerProp(r'C:\minecraft\server-Copy.properties')
     test.load()
-    print test.get('network-compression-threshold')
     test.set('network-compression-threshold', '55')
-    print test.lines
+    test.save()
